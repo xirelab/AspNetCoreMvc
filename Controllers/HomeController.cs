d7a2e715fa7f71b0b83c9d7ab76e7a090a519a05
shijuloves <shijuloves@gmail.com>
2021-10-25 22:27:00 +1100

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AspNetCoreMvc.Models;
+
+namespace AspNetCoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPrivacy()
+        {
+            return View("Privacy");
+        }
+
+        public ActionResult Admin()
+        {
+            Console.WriteLine("Admin started...");
+            return View();
+        }
+    
+        [HttpPost]
+        public ActionResult Admin(PersonModel person)
+        {
+            Console.WriteLine("Saving...");
+            int personId = person.PersonId;
+            string name = person.Name;
+            string gender = person.Gender;
+            string city = person.City;
+    
+            return View("Admin");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,21 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const button = document.getElementById('post-btn');
+
+button.addEventListener('click', async _ => {
+  try {
+    console.log('Started!');     
+    const response = await fetch('https://localhost:5001/Home/Admin', {
+      method: 'post',
+      body: {
+        // Your body
+      }
+    });
+    console.log('Completed!', response);
+  } catch(err) {
+    console.error(`Error: ${err}`);
+  }
+});
